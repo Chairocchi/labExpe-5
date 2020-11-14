e9aaa7504c4bf6d946943abaa61d1560289ab376
Allen Garcia <40850681+Chairocchi@users.noreply.github.com>
2020-11-14 18:33:08 +0800

    branch 1
    branches can be added

---
+++ b1/logout.php
@@ -0,0 +1,15 @@
+<?php
+
+ob_start();
+session_start();
+unset($_SESSION['rainbow_name']);
+unset($_SESSION['rainbow_uid']);
+unset($_SESSION['rainbow_username']);
+echo '<script type="text/javascript">window.location="../index.php"; </script>';
+
+$message = "You have been logged out.";
+		echo"<script type='text/javascript'> 
+				alert('$message');
+				window.location.href='index.php'; 
+			</script>";
+?>
