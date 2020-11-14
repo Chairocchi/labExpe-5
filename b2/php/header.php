03136ade76d426fa3a7b7327a67ae166b5d1275f
Allen Garcia <40850681+Chairocchi@users.noreply.github.com>
2020-11-14 19:16:34 +0800

    php headers

---
+++ b2/php/header.php
@@ -0,0 +1,36 @@
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <p class="navbar-brand">Normalis Novum University</p>
+            </div>
+
+        </nav>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+					<li><a class="active-menu" href="index.php"><i class="fa fa-dashboard "></i>Dashboard</a></li>
+					
+					<li><a href="student.php"><i class="fa fa-users "></i>Student</a></li>
+					
+					<li><a href="fees.php"><i class="fa fa-usd "></i>Fees</a></li>
+					
+					<li><a href="report.php"><i class="fa fa-file-text "></i>Report </a></li>
+					
+					<li><a href="setting.php"><i class="fa fa-cogs "></i>Setting</a></li>
+					
+					
+					<li><a href="logout.php" onclick="return confirm('Are you sure?')"><i class="fa fa-power-off "></i>Logout</a></li>
+					
+                </ul>
+            </div>
+        </nav>
+		<!-- /. NAV SIDE  -->
