b1263931f4f41e283fe7ba93ec9da3158cbcdff8
Allen Garcia <40850681+Chairocchi@users.noreply.github.com>
2020-11-16 11:56:06 +0800

    Update head.php

--- b1/php/head.php
+++ b1/php/head.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Normalis Novum University</title>
-		<link rel="icon" type="image/png" href="img/logo-invoice.png" />
+		<link rel="icon" type="image/png" href="img/logo.png" />
 
 		<!-- BOOTSTRAP STYLES-->
 		<link href="css/bootstrap.css" rel="stylesheet" />
